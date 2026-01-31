79b89700a72763a0fd98a77391619b4216a63e59
Sayan <s.k>
2026-01-31 14:16:34 +0500

    Issue #3.1.3

---
+++ src/RegistrationService/Version20260131102201.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NktMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260131102201 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Изменение типа good_id в таблицы sys_registration_logs';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sys_registration_logs ALTER COLUMN good_id, TYPE BIGINT DEFAULT NULL;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sys_registration_logs ALTER COLUMN good_id, TYPE BIGINT NOT NULL;');
+    }
+}
---
+++ src/RegistrationService/Version20260131102202.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NktMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260131102202 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Создание UNIQUE индекса на barcode в таблицы lst_barcodes';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX idx_lst_barcodes_barcode');
+        $this->addSql('CREATE UNIQUE INDEX idx_lst_barcodes_barcode_unique ON lst_barcodes (barcode);');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX idx_lst_barcodes_barcode_unique;');
+    }
+}
