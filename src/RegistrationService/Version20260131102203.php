f0919a485a7a587a788f4d3056ae7ba11b783091
Sayan <s.k>
2026-01-31 15:09:46 +0500

    Issue #3.1.7

---
+++ src/RegistrationService/Version20260131102203.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NktMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260131102203 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Изменение good_id на NULLABLE в таблицы sys_registration_logs';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sys_registration_logs ALTER COLUMN good_id DROP NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sys_registration_logs ALTER COLUMN good_id TYPE BIGINT NOT NULL');
+    }
+}
